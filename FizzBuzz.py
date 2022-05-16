363457a8ab4bde28c4ed800c741bd068af2ff80c
Priyanka Agarwal <priyanka81@gmail.com>
2022-05-16 21:59:53 +0100

    Update Python file

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,13 +3,11 @@ Write a Python program that prints the numbers from 1 to 100. If a number is a m
 print "Fizz" instead of the number. If the number is a multiple of five, print "Buzz" instead of the
 number. For numbers which are multiples of both three and five, print "FizzBuzz" instead of the
 number.
-
 If a number is a multiple of 7, print "Bang" instead of the number. For numbers which are
 multiples of seven and three / five, append Bang to what you'd have printed anyway. (E.g. 3 *
 7 = 21: "FizzBang").
 • If a number is a multiple of 11, print "Bong" instead of the number. Do not print anything else
 in these cases. (E.g. 3 * 11 = 33: “Bong").
-
 If a number is a multiple of 13, print "Fezz" instead of the number. For multiples of most other
 numbers, the Fezz goes immediately in front of the first thing beginning with B, or at the end if
 there are none. (E.g. 5 * 13 = 65: "FezzBuzz", 3 * 5 * 13 = 195: "FizzFezzBuzz"). Note that Fezz
